b5e97fe630da38b1e3e7c9f7ccfa7c364d351606
Aleksandr Honcharov <charleksvik@gmail.com>
2015-02-07 13:24:01 +0200

    базовый каркас

---
+++ asvdgroup/footer.php
@@ -0,0 +1,8 @@
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="<?php echo get_template_directory_uri();?>/js/bootstrap.js"></script>
+        <script>
+            $('.ph-bg-pin').tooltip();            
+        </script>
+        <?php wp_footer(); ?>
+    </body>
+</html>
---
+++ asvdgroup/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo('charset');?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="<?php bloginfo('description');?>">    
+        <title><?php the_title(); ?> | <?php bloginfo('name');?></title>
+        <link rel="shortcut icon" href="">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/bootstrap.css">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/bootstrap-theme.css">
+
+        <!--[if IE]>
+            <script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        <?php if ( is_user_logged_in() ) {echo '<style>.navbar.navbar-inverse.navbar-fixed-top {margin-top:32px;}</style>';} ?>
+        <?php wp_head(); ?>  
+    </head>
